f3685ac8e7a33cae4b11e9f79e9a53977d93bd03
Zhandos04 <nurbekulyzhandos@gmail.com>
2024-11-21 19:11:45 +0500

    first commit

---
+++ src/main/java/backend/project/sdulik/SdUlikApplication.java
@@ -0,0 +1,13 @@
+package backend.project.sdulik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdUlikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdUlikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backend/project/sdulik/SdUlikApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.project.sdulik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdUlikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
